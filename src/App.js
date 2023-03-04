ae68e928f9b86aaeddc200c851a1576849d3068c
Manish Guhe <manishguhe301@gmail.com>
2023-03-04 18:11:39 +0530

    removed comments

--- src/App.js
+++ src/App.js
@@ -12,23 +12,3 @@ const App = () => {
 
 export default App;
 
-// The exact part of the section to build has been marked here:
-// https://i.imgur.com/EU9IjCB.png
-
-// Figma link to About Us Page design:
-// https://www.figma.com/file/2wzCVWBJOrib8y6do75jrx/%E2%9D%96-Untitled-UI-%E2%80%93-FREE-version-(Community)?node-id=1622%3A261463&t=S7qyx6GsJSLm7U4J-0
-
-// Restrictions: Do not use a CSS framework. Write CSS from scratch. You may, however, use a CSS reset of your choice (not compulsory).
-
-// We are looking for
-// - How you structure your HTML
-// - The class names you create & use
-// - Coding style (indents)
-// - Decisions you take after seeing the layout
-// - Any specific decisions you take if you foresee an issue.
-// - Assume you only have to code for the following two device types
-//   - Mobile (use mobile-first approach to using media queries or better if you know)
-//   - Tablet/Desktop @media (min-width: 768px) { ... }
-
-// - We are also looking for how comfortable you are using the code editor of your choice
-// - Any other tools that you use to speed up your work.
